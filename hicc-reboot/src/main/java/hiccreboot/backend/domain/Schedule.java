d3660faa12181ca735e1ce0a8fa398beae7ae2f1
yooooonshine <hl3470@naver.com>
2024-01-10 20:31:35 +0900

    feat: Schedule엔티티 및 SchduleDate 엔티티 구현, ScheduleType Enum 구현

--- hicc-reboot/src/main/java/hiccreboot/backend/domain/Schedule.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/Schedule.java
@@ -1,4 +1,38 @@
 package hiccreboot.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SCHEDULE_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated
+    private ScheduleType scheduleType;
+
+    @OneToMany
+    @JoinColumn(name = "SCHEDULE_ID")
+    private List<ScheduleDate> scheduleDates = new ArrayList<>();
+
+    //연관관계 메소드
+    public void addScheduleDate(ScheduleDate scheduleDate) {
+        this.scheduleDates.add(scheduleDate);
+        scheduleDate.setSchedule(this);
+    }
 }
+
