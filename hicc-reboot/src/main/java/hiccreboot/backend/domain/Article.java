15d8aac8a2941d503c4037b1293e9af977687696
yooooonshine <hl3470@naver.com>
2024-01-10 14:19:55 +0900

    feat: member엔티티 구현 및 BoardType Enum 구현

--- hicc-reboot/src/main/java/hiccreboot/backend/domain/Article.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/Article.java
@@ -1,4 +1,60 @@
 package hiccreboot.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+
+    @Id
+    @Column(name = "ARTICLE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ARTICLE_ID")
+    private Member member;
+
+    @Column(nullable = false)
+    private String subject;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated
+    @Column(nullable = false)
+    private BoardType boardType;
+
+    @Builder
+    private Article(Member member, String subject, String content, BoardType boardType) {
+        this.id = id;
+        this.member = member;
+        this.subject = subject;
+        this.content = content;
+        this.boardType = boardType;
+    }
+
+    public Article createArticle(Member member, String subject, String content, BoardType boardType) {
+        return Article.builder()
+                .member(member)
+                .subject(subject)
+                .content(content)
+                .boardType(boardType)
+                .build();
+    }
+
+    public void updateSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void updateBoardType(BoardType boardType) {
+        this.boardType = boardType;
+    }
 }
