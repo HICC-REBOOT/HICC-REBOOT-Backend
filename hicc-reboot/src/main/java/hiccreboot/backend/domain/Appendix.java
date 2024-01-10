2f1ad18ac34c754b7e3e7ac656a12d2d2f0d5bff
yooooonshine <hl3470@naver.com>
2024-01-10 20:59:45 +0900

    feat: Appendix 엔티티 구현

--- hicc-reboot/src/main/java/hiccreboot/backend/domain/Appendix.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/Appendix.java
@@ -1,4 +1,30 @@
 package hiccreboot.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Appendix {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "APPENDIX_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String path;
+
+    @Builder
+    private Appendix(String path) {
+        this.path = path;
+    }
+
+    public Appendix createAppendix(String path) {
+        return Appendix.builder()
+                .path(path)
+                .build();
+    }
 }
