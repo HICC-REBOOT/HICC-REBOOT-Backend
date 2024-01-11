b48b30c98e2fb9f62be161f4269f8865720dd1cd
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2024-01-11 15:40:35 +0900

    feature: College엔티티 구현 및 Department 엔티티구현 (#12) (#13)
    * feature: cutom 예외 생성 (#7) (#8)
    
    * feat: error code 생성
    
    * feat: error reason 생성
    
    * feat: HttpConstants 생성
    
    * feat: BaseResponse 생성
    
    * feat: ErrorResponse 구현
    
    * feat: CustomException 구현
    
    * feat: GlobalExceptionHandler 구현
    
    * feat: College엔티티 구현 및 Department 엔티티구현
    
    Collage엔티티와 Department 엔티티를 구현하여, 연관관계를 설정하였고,
    Member엔티티에 Department와 College에 대하여 연관관계 메서드를 구현하였다.
    
    * refactoring:지연로딩 설정
    
    College에 있는 Department와 Member에 있는 College, Department에 대해 지연로딩 설정을 추가하였다.
    
    * refactoring: College, Department의 name에 대한 unique 제약 조건 추가
    
    ---------
    
    Co-authored-by: Lee-sh98 <55966515+Lee-sh98@users.noreply.github.com>

--- hicc-reboot/src/main/java/hiccreboot/backend/domain/Member.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/Member.java
@@ -1,81 +1,98 @@
 package hiccreboot.backend.domain;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-	@Id
-	@Column(name = "member_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(unique = true, nullable = false)
-	private String studentNumber;
+    @Column(unique = true, nullable = false)
+    private String studentNumber;
 
-	@Column(nullable = false)
-	private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    @JoinColumn(name = "DEPARTMENT_ID")
+    private Department department;
 
-	@Column(nullable = false)
-	private String password;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    @JoinColumn(name = "COLLEGE_ID")
+    private College college;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private Grade grade;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false)
-	private String phoneNumber;
+    @Column(nullable = false)
+    private String password;
 
-	private String refreshToken;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Grade grade;
 
-	@Builder
-	private Member(String studentNumber, String name, String password, Grade grade, String phoneNumber) {
-		this.studentNumber = studentNumber;
-		this.name = name;
-		this.password = password;
-		this.grade = grade;
-		this.phoneNumber = phoneNumber;
-	}
+    @Column(nullable = false)
+    private String phoneNumber;
 
-	public static Member createMember(String studentNumber, String name, String password, Grade grade,
-		String phoneNumber) {
-		return Member.builder()
-			.studentNumber(studentNumber)
-			.name(name)
-			.password(password)
-			.grade(grade)
-			.phoneNumber(phoneNumber)
-			.build();
-	}
+    private String refreshToken;
 
-	public void updateGrade(Grade grade) {
-		this.grade = grade;
-	}
+    @Builder
+    private Member(String studentNumber, Department department, String name, String password, Grade grade, String phoneNumber) {
+        this.studentNumber = studentNumber;
+        this.department = department;
+        this.name = name;
+        this.password = password;
+        this.grade = grade;
+        this.phoneNumber = phoneNumber;
+    }
 
-	public void passwordEncode(PasswordEncoder passwordEncoder) {
-		this.password = passwordEncoder.encode(this.password);
-	}
+    public static Member createMember(String studentNumber, Department department, String name, String password, Grade grade,
+                                      String phoneNumber) {
+        return Member.builder()
+                .studentNumber(studentNumber)
+                .department(department)
+                .name(name)
+                .password(password)
+                .grade(grade)
+                .phoneNumber(phoneNumber)
+                .build();
+    }
 
-	public void updateRefreshToken(String updateRefreshToken) {
-		this.refreshToken = updateRefreshToken;
-	}
+    public void updateGrade(Grade grade) {
+        this.grade = grade;
+    }
 
-	public void destroyRefreshToken() {
-		this.refreshToken = null;
-	}
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+
+    public void destroyRefreshToken() {
+        this.refreshToken = null;
+    }
+
+    //연관 관계 메서드
+    public void changeDepartment(Department department) {
+        this.department = department;
+        department.getMembers().add(this);
+    }
+
+    public void changeCollege(College college) {
+        this.college = college;
+        college.getMembers().add(this);
+    }
+
 
 }
