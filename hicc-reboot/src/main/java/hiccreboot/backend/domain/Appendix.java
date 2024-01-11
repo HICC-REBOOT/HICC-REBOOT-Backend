417068cabc94cfb0c672c3e581e085ce04a9ccf0
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2024-01-11 15:41:35 +0900

    feat: Appendix 엔티티 구현 (#16) (#17)
    * feat: Appendix 엔티티 구현
    
    * refactoring: Appendix엔티티에 Article 연관관계 일대다 양방향 설정

--- hicc-reboot/src/main/java/hiccreboot/backend/domain/Appendix.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/Appendix.java
@@ -1,4 +1,40 @@
 package hiccreboot.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Appendix {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "APPENDIX_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appendix", insertable = false, updatable = false)
+    @Column(nullable = false)
+    private Article article;
+
+    @Builder
+    private Appendix(String path, Article article) {
+        this.path = path;
+        this.article = article;
+    }
+
+    public Appendix createAppendix(String path) {
+        return Appendix.builder()
+                .path(path)
+                .build();
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
 }
