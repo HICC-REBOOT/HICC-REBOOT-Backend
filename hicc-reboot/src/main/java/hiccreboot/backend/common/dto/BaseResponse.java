e32702c71f45df718fa524e8badf00daad8d3749
leesh <qwqw8019@naver.com>
2024-01-17 17:54:41 +0900

    feat: base response 내 timestamp 포맷팅

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/BaseResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/BaseResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatusCode;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Getter;
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse {
+	@JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
 	private final LocalDateTime timestamp = LocalDateTime.now();
 	private final boolean isSuccess;
 	private final HttpStatusCode statusCode;
