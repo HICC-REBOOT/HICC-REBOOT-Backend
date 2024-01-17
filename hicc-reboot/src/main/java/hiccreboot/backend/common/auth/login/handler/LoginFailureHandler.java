48a87aafe5ee44db50ebdac528da82a12c9877d8
leesh <qwqw8019@naver.com>
2024-01-17 13:35:10 +0900

    feat: 로그인 실패 핸들러 구현

---
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/auth/login/handler/LoginFailureHandler.java
@@ -0,0 +1,25 @@
+package hiccreboot.backend.common.auth.login.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) {
+		response.setStatus(HttpServletResponse.SC_OK);//보안을 위해 로그인 오류지만 200 반환
+
+		response.setHeader("success", "false");
+		response.setHeader("timestamp", LocalDateTime.now().toString());
+	}
+}
