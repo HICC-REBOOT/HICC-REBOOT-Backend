95b22c7b72c90ca18e7a836d1ab99c054371213c
leesh <qwqw8019@naver.com>
2024-01-17 22:21:02 +0900

    feat: auth controller 생성

---
+++ hicc-reboot/src/main/java/hiccreboot/backend/controller/AuthController.java
@@ -0,0 +1,45 @@
+package hiccreboot.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hiccreboot.backend.common.auth.jwt.TokenProvider;
+import hiccreboot.backend.common.dto.BaseResponse;
+import hiccreboot.backend.common.dto.DataResponse;
+import hiccreboot.backend.dto.request.SignUpRequest;
+import hiccreboot.backend.dto.request.StudentNumberCheckRequest;
+import hiccreboot.backend.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final MemberService memberService;
+	private final TokenProvider tokenProvider;
+
+	@PostMapping("/sign-up")
+	public BaseResponse signUp(@RequestBody SignUpRequest request) {
+		memberService.signUp(request);
+		return DataResponse.ok();
+	}
+
+	@PostMapping("/duplicate")
+	public BaseResponse checkDuplicate(@RequestBody StudentNumberCheckRequest request) {
+		return memberService.checkDuplicate(request);
+	}
+
+	@GetMapping("/info")
+	public BaseResponse userInfo(HttpServletRequest servletRequest) {
+		String studentNumber = tokenProvider.extractStudentNumber(servletRequest)
+			.orElse(null);
+		return memberService.findSimpleInfo(studentNumber);
+	}
+}
