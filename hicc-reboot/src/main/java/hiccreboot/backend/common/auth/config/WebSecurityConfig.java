0d775d5ca7ef5b0edc921df32c6a45bd500bf358
leesh <qwqw8019@naver.com>
2024-01-17 17:16:56 +0900

    feat: RefreshToken 기본 생성자 추가

--- hicc-reboot/src/main/java/hiccreboot/backend/common/auth/config/WebSecurityConfig.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/auth/config/WebSecurityConfig.java
@@ -4,6 +4,9 @@ import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,24 +14,40 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hiccreboot.backend.common.auth.jwt.TokenProvider;
+import hiccreboot.backend.common.auth.jwt.filter.JwtAuthenticationFilter;
+import hiccreboot.backend.common.auth.login.filter.StudentNumberPasswordAuthenticationFilter;
+import hiccreboot.backend.common.auth.login.handler.LoginFailureHandler;
+import hiccreboot.backend.common.auth.login.handler.LoginSuccessHandler;
+import hiccreboot.backend.common.auth.login.service.LoginService;
+import hiccreboot.backend.repository.member.MemberRepository;
+import hiccreboot.backend.repository.refreshToken.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-	private static AntPathRequestMatcher[] PERMIT_ALL_PATTERNS() {
-		String[] PATTERNS = new String[] {
-			"api/login/**",
-			"api/sing-up/**",
-			"api/logout/**"
-		};
-		return Stream.of(PATTERNS)
-			.map(AntPathRequestMatcher::antMatcher)
-			.toArray(AntPathRequestMatcher[]::new);
-	}
+
+	private final TokenProvider tokenProvider;
+	private final MemberRepository memberRepository;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final ObjectMapper objectMapper;
+	private final LoginService loginService;
+
+	private final LoginSuccessHandler loginSuccessHandler;
+	private final LoginFailureHandler loginFailureHandler;
+
+	private static String[] ALLOWED_PATTERN = new String[] {
+		"/api/login/**",
+		"/api/sign-up**",
+		"/api/logout**",
+	};
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -38,11 +57,21 @@ public class WebSecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-				.requestMatchers(PERMIT_ALL_PATTERNS())
+				.requestMatchers(Stream.of(ALLOWED_PATTERN)
+					.map(AntPathRequestMatcher::antMatcher)
+					.toArray(AntPathRequestMatcher[]::new))
 				.permitAll()
 				.anyRequest()
-				.permitAll()) // for develop
-			// .authenticated())
+				.authenticated()) // for develop
+			.logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+				.logoutUrl("/api/logout")
+				.logoutSuccessHandler((request, response, authentication) -> {
+					tokenProvider.extractStudentNumber(request)
+						.ifPresent(tokenProvider::disableRefreshToken);
+				})
+				.deleteCookies())
+			.addFilterAfter(jwtAuthenticationFilter(), LogoutFilter.class)
+			.addFilterAfter(studentNumberPasswordAuthenticationFilter(), JwtAuthenticationFilter.class)
 			.build();
 	}
 
@@ -50,4 +79,32 @@ public class WebSecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(tokenProvider, memberRepository, refreshTokenRepository);
+	}
+
+	@Bean
+	public StudentNumberPasswordAuthenticationFilter studentNumberPasswordAuthenticationFilter() {
+		StudentNumberPasswordAuthenticationFilter authenticationFilter = new StudentNumberPasswordAuthenticationFilter(
+			objectMapper);
+
+		authenticationFilter.setAuthenticationManager(authenticationManager());
+		authenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler);
+		authenticationFilter.setAuthenticationFailureHandler(loginFailureHandler);
+
+		return authenticationFilter;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+		provider.setUserDetailsService(loginService);
+		provider.setPasswordEncoder(passwordEncoder());
+
+		return new ProviderManager(provider);
+	}
+
 }
--- hicc-reboot/src/main/java/hiccreboot/backend/domain/RefreshToken.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domain/RefreshToken.java
@@ -5,22 +5,21 @@ import org.springframework.data.redis.core.index.Indexed;
 
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @RedisHash(value = "refresh", timeToLive = 60 * 60 * 24)
 public class RefreshToken {
 	@Id
 	private Long id;
 	@Indexed
 	private String token;
-
-	@Builder(access = AccessLevel.PRIVATE)
-	private RefreshToken(Long id, String token) {
-		this.id = id;
-		this.token = token;
-	}
 
 	public static RefreshToken createRefreshToken(Member member, String token) {
 		return RefreshToken.builder()
