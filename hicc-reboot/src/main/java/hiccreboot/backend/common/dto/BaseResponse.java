b4606fdce62d48f83f6e7ef74d194ad712e69da1
leesh <qwqw8019@naver.com>
2024-01-19 02:06:43 +0900

    feat: 기본 반환 클래스 swagger 적용

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/BaseResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/BaseResponse.java
@@ -7,14 +7,17 @@ import org.springframework.http.HttpStatusCode;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse {
 	@JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
+	@Schema(example = "2024-01-00 13:00:00", type = "string")
 	private final LocalDateTime timestamp = LocalDateTime.now();
 	private final boolean isSuccess;
+	@Schema(example = "OK")
 	private final HttpStatusCode statusCode;
 
 	protected BaseResponse(boolean isSuccess, HttpStatusCode statusCode) {
