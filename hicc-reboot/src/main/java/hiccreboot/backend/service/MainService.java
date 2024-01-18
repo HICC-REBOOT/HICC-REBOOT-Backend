81a830914fa673fb654316b9985b548a9a2cf74c
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2024-01-18 17:58:39 +0900

    feat: Main페이지 관련 Service, Controller 구현(#38) (#39)
    * feat: Main페이지 관련 Service, Controller 구현
    
    Main페이지 동아리 통계 기능, 최신 소식 목록 조회기능 구현
    
    * refator: Main Controller에 jwt와 response 객체 구현
    
    * refator: Main Controller에 jwt와 response 객체 구현
    
    * refactor: article 작자, 등급 수정
    
    기존의 jwt에서 작자, 등급을 가져왔던 점에서, article 내부에서 가져오도록 수정

---
+++ hicc-reboot/src/main/java/hiccreboot/backend/service/MainService.java
@@ -0,0 +1,46 @@
+package hiccreboot.backend.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import hiccreboot.backend.common.dto.DataResponse;
+import hiccreboot.backend.common.dto.Main.LatestNewsResponse;
+import hiccreboot.backend.common.exception.ArticleNotFoundException;
+import hiccreboot.backend.domain.Article;
+import hiccreboot.backend.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MainService {
+
+	private final MemberRepository memberRepository;
+	private final ArticleService articleService;
+
+	public Long findMemberCount() {
+		return memberRepository.count();
+	}
+
+	public DataResponse<List<LatestNewsResponse>> makeLatestNews(int pageNumber, int pageSize) {
+		List<Article> articles = articleService.findArticles(pageNumber, pageSize).getContent();
+
+		if (articles.isEmpty()) {
+			throw ArticleNotFoundException.EXCEPTION;
+		}
+
+		List<LatestNewsResponse> latestNewsResponses = new ArrayList<>();
+		articles.stream()
+			.forEach(article -> latestNewsResponses.add(new LatestNewsResponse(
+				article.getId(),
+				article.getMember().getGrade(),
+				article.getMember().getName(),
+				article.getDate(),
+				!article.getAppendices().isEmpty(),
+				article.getSubject())));
+		return DataResponse.ok(latestNewsResponses);
+	}
+}
