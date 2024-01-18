ad1c8b6e16c087d0e5f4f06dca71286c5f91dd76
yooooonshine <hl3470@naver.com>
2024-01-18 17:11:41 +0900

    refactor: article 작자, 등급 수정
    기존의 jwt에서 작자, 등급을 가져왔던 점에서, article 내부에서 가져오도록 수정

--- hicc-reboot/src/main/java/hiccreboot/backend/controller/MainController.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/controller/MainController.java
@@ -1,17 +1,12 @@
 package hiccreboot.backend.controller;
 
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import hiccreboot.backend.common.auth.jwt.TokenProvider;
 import hiccreboot.backend.common.dto.DataResponse;
-import hiccreboot.backend.domain.Member;
 import hiccreboot.backend.service.MainService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,25 +15,19 @@ import lombok.RequiredArgsConstructor;
 public class MainController {
 
 	private final MainService mainService;
-	private final MemberService memberService;
-	private final TokenProvider tokenProvider;
 
 	@GetMapping("/member-count")
-	public Object searchMemberCount() {
+	public DataResponse<Long> searchMemberCount() {
 		Long memberCount = mainService.findMemberCount();
 
 		return DataResponse.ok(memberCount);
 	}
 
 	@GetMapping("/latest-news")
-	public Object searchLatestNews(
+	public DataResponse searchLatestNews(
 		@RequestParam(value = "page") int pageNumber,
-		@RequestParam(value = "size") int pageSize,
-		HttpServletRequest httpServletRequest) {
-		Optional<String> studentNumber = tokenProvider.extractStudentNumber(httpServletRequest);
-		Member member = memberService.findMemberByStudentNumber(studentNumber.get());
-
-		return mainService.makeLatestNews(pageNumber, pageSize, member.getGrade(), member.getName());
+		@RequestParam(value = "size") int pageSize) {
+		return mainService.makeLatestNews(pageNumber, pageSize);
 	}
 }
 
--- hicc-reboot/src/main/java/hiccreboot/backend/service/MainService.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/service/MainService.java
@@ -4,27 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import hiccreboot.backend.common.dto.DataListResponse;
+import hiccreboot.backend.common.dto.DataResponse;
 import hiccreboot.backend.common.dto.Main.LatestNewsResponse;
 import hiccreboot.backend.common.exception.ArticleNotFoundException;
 import hiccreboot.backend.domain.Article;
-import hiccreboot.backend.domain.Grade;
+import hiccreboot.backend.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MainService {
 
 	private final MemberRepository memberRepository;
 	private final ArticleService articleService;
 
 	public Long findMemberCount() {
-		return MemberRepository.countBy();
+		return memberRepository.count();
 	}
 
-	public DataListResponse<LatestNewsResponse> makeLatestNews(int pageNumber, int pageSize, Grade memberGrade,
-		String memberName) {
+	public DataResponse<List<LatestNewsResponse>> makeLatestNews(int pageNumber, int pageSize) {
 		List<Article> articles = articleService.findArticles(pageNumber, pageSize).getContent();
 
 		if (articles.isEmpty()) {
@@ -35,11 +36,11 @@ public class MainService {
 		articles.stream()
 			.forEach(article -> latestNewsResponses.add(new LatestNewsResponse(
 				article.getId(),
-				memberGrade,
-				memberName,
+				article.getMember().getGrade(),
+				article.getMember().getName(),
 				article.getDate(),
 				!article.getAppendices().isEmpty(),
 				article.getSubject())));
-		return DataListResponse.create(latestNewsResponses);
+		return DataResponse.ok(latestNewsResponses);
 	}
 }
