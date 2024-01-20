3f61d464002ad529644c004eaff978dd5a4f2471
leesh <qwqw8019@naver.com>
2024-01-20 18:00:35 +0900

    feat: 전역 예외 처리

--- hicc-reboot/src/main/java/hiccreboot/backend/common/exception/GlobalExceptionHandler.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/exception/GlobalExceptionHandler.java
@@ -5,12 +5,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import hiccreboot.backend.common.exception.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
 	public ErrorResponse customExceptionHandler(CustomException e, HttpServletRequest request) {
 		return ErrorResponse.fromCustomException(e, request.getRequestURI());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ErrorResponse handleException(Exception e, HttpServletRequest request) {
+		log.error("INTERNAL_SERVER_ERROR: ", e);
+		BaseErrorCode errorCode = GlobalErrorCode.INTERNAL_SERVER_EXCEPTION;
+		String path = request.getRequestURI();
+
+		return ErrorResponse.fromErrorCode(errorCode, path);
 	}
 }
