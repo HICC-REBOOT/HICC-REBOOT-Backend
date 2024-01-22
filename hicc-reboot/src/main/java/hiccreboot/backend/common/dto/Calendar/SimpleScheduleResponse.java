97f8347ab69da72fb3b4eee5d152b497b6215561
yooooonshine <hl3470@naver.com>
2024-01-22 12:33:20 +0900

    Refactor: @Date 제거

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/SimpleScheduleResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/SimpleScheduleResponse.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import hiccreboot.backend.domain.ScheduleDate;
 import hiccreboot.backend.domain.ScheduleType;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 
-@Data
-@RequiredArgsConstructor
+@Getter
 public class SimpleScheduleResponse {
 	private final String name;
 	private final Long scheduleId;
@@ -17,5 +18,21 @@ public class SimpleScheduleResponse {
 	private final LocalDate date;
 	private final ScheduleType type;
 
+	@Builder(access = AccessLevel.PRIVATE)
+	private SimpleScheduleResponse(String name, Long scheduleId, LocalDate date, ScheduleType type) {
+		this.name = name;
+		this.scheduleId = scheduleId;
+		this.date = date;
+		this.type = type;
+	}
+
+	public static SimpleScheduleResponse create(ScheduleDate scheduleDate) {
+		return SimpleScheduleResponse.builder()
+			.name(scheduleDate.getSchedule().getName())
+			.scheduleId(scheduleDate.getSchedule().getId())
+			.date(LocalDate.of(scheduleDate.getYear(), scheduleDate.getMonth(), scheduleDate.getDayOfMonth()))
+			.type(scheduleDate.getSchedule().getScheduleType())
+			.build();
+	}
 }
 
--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/UpdateScheduleRequest.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/UpdateScheduleRequest.java
@@ -7,15 +7,12 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import hiccreboot.backend.domain.ScheduleType;
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
-@Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UpdateScheduleRequest {
 
-	private Long scheduleId;
 	private String name;
 	private List<LocalDate> dates;
 	private ScheduleType type;
