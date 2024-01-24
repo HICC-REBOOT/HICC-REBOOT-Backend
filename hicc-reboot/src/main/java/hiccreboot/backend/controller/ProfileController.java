ed922073329b27ce00b263b9dd04111d73867e60
leesh <qwqw8019@naver.com>
2024-01-24 18:01:03 +0900

    feat: 사용자별 작성글, 작성 댓글 기능 적용

--- hicc-reboot/src/main/java/hiccreboot/backend/controller/ProfileController.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/controller/ProfileController.java
@@ -1,16 +1,20 @@
 package hiccreboot.backend.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hiccreboot.backend.common.auth.jwt.TokenProvider;
 import hiccreboot.backend.common.dto.BaseResponse;
 import hiccreboot.backend.common.dto.DataResponse;
 import hiccreboot.backend.dto.request.ProfileModifyRequest;
+import hiccreboot.backend.dto.response.PersonalArticleResponse;
+import hiccreboot.backend.dto.response.PersonalCommentResponse;
 import hiccreboot.backend.dto.response.ProfileMemberResponse;
 import hiccreboot.backend.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -49,5 +53,20 @@ public class ProfileController {
 
 		return DataResponse.noContent();
 	}
-	
+
+	@GetMapping("/articles")
+	public DataResponse<Page<PersonalArticleResponse>> findPersonalArticles(@RequestParam(value = "page") int page,
+		@RequestParam(value = "size") int size, HttpServletRequest servletRequest) {
+		String studentNumber = tokenProvider.extractStudentNumber(servletRequest).get();
+
+		return memberService.findPersonalArticles(page, size, studentNumber);
+	}
+
+	@GetMapping("/comments")
+	public DataResponse<Page<PersonalCommentResponse>> findPersonalComments(@RequestParam(value = "page") int page,
+		@RequestParam(value = "size") int size, HttpServletRequest servletRequest) {
+		String studentNumber = tokenProvider.extractStudentNumber(servletRequest).get();
+
+		return memberService.findPersonalComments(page, size, studentNumber);
+	}
 }
