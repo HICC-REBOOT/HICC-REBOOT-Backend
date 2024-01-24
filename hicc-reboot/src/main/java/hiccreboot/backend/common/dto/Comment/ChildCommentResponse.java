f15db4534967ad103e90906313704391fbb2a64e
yooooonshine <hl3470@naver.com>
2024-01-24 15:17:55 +0900

    feat: CommentResponse에 grade 추가

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/ChildCommentResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/ChildCommentResponse.java
@@ -1,19 +1,22 @@
-package hiccreboot.backend.common.dto.ParentComment;
+package hiccreboot.backend.common.dto.Comment;
 
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import hiccreboot.backend.domain.Grade;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class ParentCommentResponse {
+public class ChildCommentResponse {
 
 	private final Long articleId;
+	private final Long parentCommentId;
 	private final Long commentId;
 	private final String name;
+	private final Grade grade;
 	private final Boolean isMine;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
 	private final LocalDateTime date;
