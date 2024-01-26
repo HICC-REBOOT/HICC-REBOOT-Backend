3e288114d1d7ffdf03e53e093c2fd2f50cc1d8e9
yooooonshine <hl3470@naver.com>
2024-01-26 14:09:44 +0900

    refactor: request에 JsonNaming 제거

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/PostCommentRequest.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/PostCommentRequest.java
@@ -1,12 +1,8 @@
 package hiccreboot.backend.common.dto.Comment;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import lombok.Getter;
 
 @Getter
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PostCommentRequest {
 
 	private Long articleId;
