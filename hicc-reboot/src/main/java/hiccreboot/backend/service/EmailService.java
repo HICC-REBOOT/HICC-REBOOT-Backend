daede30fd7a405a2839cb4454b1e0a0fb70822de
leesh <qwqw8019@naver.com>
2024-01-26 16:05:38 +0900

    refactor: 이메일 전송 로직 개선

--- hicc-reboot/src/main/java/hiccreboot/backend/service/EmailService.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/service/EmailService.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import hiccreboot.backend.repository.member.MemberRepository;
@@ -20,31 +21,33 @@ public class EmailService {
 
 	private final JavaMailSender javaMailSender;
 	private final MemberRepository memberRepository;
+	private final SimpleMailMessage template;
+	private final PasswordEncoder passwordEncoder;
 
 	private static final String SUBJECT = "HICC password reissue";
-	private static final String CONTENT = "Temporary password is reissued. Change your password. Reissued Password: ";
+	private static final String CONTENT = "임시 비밀번호가 발급되었습니다.\n반드시 비밀번호를 변경해주세요.\nReissued Password:%s";
 
 	public void sendTempPassword(String studentNumber, String email) {
 		memberRepository.findByStudentNumber(studentNumber)
+			.filter(member -> member.getEmail().equals(email))
 			.ifPresent(member -> {
-				if (member.getEmail().equals(email)) {
-					String reissuedPassword = makeTempNumber();
+				String reissuedPassword = makeTempNumber();
 
-					SimpleMailMessage mail = createEmail(member.getEmail(), reissuedPassword);
+				SimpleMailMessage mail = createEmail(member.getEmail(), reissuedPassword);
 
 					javaMailSender.send(mail);
 				}
 			});
-
 	}
 
 	private SimpleMailMessage createEmail(String receiver, String reissuedPassword) {
 
-		SimpleMailMessage message = new SimpleMailMessage();
+		SimpleMailMessage message = new SimpleMailMessage(template);
 
 		message.setTo(receiver);
 		message.setSubject(SUBJECT);
-		message.setText(CONTENT + reissuedPassword);
+		String text = String.format(CONTENT, reissuedPassword);
+		message.setText(text);
 
 		return message;
 	}
