d1ccda690c73c2afe59cb7d9a48fd86db9fccdce
yooooonshine <hl3470@naver.com>
2024-01-29 16:41:46 +0900

    feat: CalendarService update, delete, save에 권한 확인 로직 추가

--- hicc-reboot/src/main/java/hiccreboot/backend/service/CalendarService.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/service/CalendarService.java
@@ -9,16 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import hiccreboot.backend.common.dto.BaseResponse;
+import hiccreboot.backend.common.dto.Calendar.PostScheduleRequest;
 import hiccreboot.backend.common.dto.Calendar.ScheduleResponse;
 import hiccreboot.backend.common.dto.Calendar.SimpleScheduleResponse;
 import hiccreboot.backend.common.dto.Calendar.UpdateScheduleRequest;
 import hiccreboot.backend.common.dto.DataResponse;
+import hiccreboot.backend.common.exception.AccessForbiddenException;
+import hiccreboot.backend.common.exception.MemberNotFoundException;
 import hiccreboot.backend.common.exception.ScheduleNotFoundException;
+import hiccreboot.backend.domain.Grade;
+import hiccreboot.backend.domain.Member;
 import hiccreboot.backend.domain.Schedule;
 import hiccreboot.backend.domain.ScheduleDate;
-import hiccreboot.backend.domain.ScheduleType;
 import hiccreboot.backend.repository.Calender.ScheduleDateRepository;
 import hiccreboot.backend.repository.Calender.ScheduleRepository;
+import hiccreboot.backend.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -28,6 +33,7 @@ public class CalendarService {
 
 	private final ScheduleRepository scheduleRepository;
 	private final ScheduleDateRepository scheduleDateRepository;
+	private final MemberRepository memberRepository;
 
 	private List<ScheduleDate> findScheduleDatesByMonth(int year, int month) {
 		return scheduleDateRepository.findAllByYearAndMonth(year, month);
@@ -64,9 +70,15 @@ public class CalendarService {
 	}
 
 	@Transactional
-	public Schedule saveSchedule(String name, List<LocalDate> dates, String content, ScheduleType type) {
-		Schedule schedule = Schedule.createSchedule(name, content, type);
+	public Schedule saveSchedule(String studentNumber, PostScheduleRequest postScheduleRequest) {
+		Member member = memberRepository.findByStudentNumber(studentNumber)
+			.orElseThrow(() -> MemberNotFoundException.EXCEPTION);
 
+		checkCalendarAuthority(member.getGrade());
+
+		Schedule schedule = Schedule.createSchedule(postScheduleRequest);
+
+		List<LocalDate> dates = postScheduleRequest.getDates();
 		dates.stream()
 			.forEach(date -> ScheduleDate.create(date, schedule));
 		scheduleRepository.save(schedule);
@@ -75,12 +87,22 @@ public class CalendarService {
 	}
 
 	@Transactional
-	public void deleteSchedule(Long id) {
+	public void deleteSchedule(String studentNumber, Long id) {
+		Member member = memberRepository.findByStudentNumber(studentNumber)
+			.orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+
+		checkCalendarAuthority(member.getGrade());
+
 		scheduleRepository.deleteById(id);
 	}
 
 	@Transactional
-	public Schedule updateSchedule(Long id, UpdateScheduleRequest updateScheduleRequest) {
+	public Schedule updateSchedule(String studentNumber, Long id, UpdateScheduleRequest updateScheduleRequest) {
+		Member member = memberRepository.findByStudentNumber(studentNumber)
+			.orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+
+		checkCalendarAuthority(member.getGrade());
+
 		Schedule schedule = findSchedule(id).orElseThrow(() -> ScheduleNotFoundException.EXCEPTION);
 
 		//schedule 변경
@@ -95,5 +117,12 @@ public class CalendarService {
 			});
 
 		return schedule;
+	}
+
+	private void checkCalendarAuthority(Grade grade) {
+		if (grade == Grade.EXECUTIVE || grade == Grade.PRESIDENT) {
+			return;
+		}
+		throw AccessForbiddenException.EXCEPTION;
 	}
 }
