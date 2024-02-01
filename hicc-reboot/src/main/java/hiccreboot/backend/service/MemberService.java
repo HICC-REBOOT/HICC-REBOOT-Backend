0a17bd3fe480627bb169b7053035a68871f82a0f
leesh <qwqw8019@naver.com>
2024-02-01 14:14:39 +0900

    feat: 내 작성 댓글 최신순 정렬

--- hicc-reboot/src/main/java/hiccreboot/backend/service/MemberService.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/service/MemberService.java
@@ -227,7 +227,7 @@ public class MemberService {
 	}
 
 	public DataResponse<Page<PersonalCommentResponse>> findPersonalComments(int page, int size, String studentNumber) {
-		Pageable pageable = PageRequest.of(page, size);
+		Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
 
 		Member member = memberRepository.findByStudentNumber(studentNumber)
 			.orElseThrow(() -> MemberNotFoundException.EXCEPTION);
