7b79f26322c602a2884fd3d4f7fa5669b2c95344
yooooonshine <hl3470@naver.com>
2024-02-02 14:44:00 +0900

    refactor: comment의 name을 memberName을 전달하도록 변경

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/ParentCommentResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Comment/ParentCommentResponse.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import hiccreboot.backend.domain.Comment;
-import hiccreboot.backend.domain.Grade;
+import hiccreboot.backend.domain.CommentGrade;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,7 +16,7 @@ public class ParentCommentResponse {
 	private final Long articleId;
 	private final Long commentId;
 	private final String name;
-	private final Grade grade;
+	private final CommentGrade grade;
 	private final Boolean isMine;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
 	private final LocalDateTime date;
@@ -25,8 +25,8 @@ public class ParentCommentResponse {
 	public ParentCommentResponse(Comment comment, Boolean isMine) {
 		this.articleId = comment.getArticle().getId();
 		this.commentId = comment.getId();
-		this.name = comment.getMember().getName();
-		this.grade = comment.getMember().getGrade();
+		this.name = comment.getMemberName();
+		this.grade = comment.getCommentGrade();
 		this.isMine = isMine;
 		this.date = comment.getDate();
 		this.content = comment.getContent();
