94ae3e9173a5b5b19f230e1a2d9bb0b8337f4bde
leesh <qwqw8019@naver.com>
2024-02-07 17:28:25 +0900

    feat: 시작 시간, 끝 시간 추가

--- hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/ScheduleDateResponse.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/common/dto/Calendar/ScheduleDateResponse.java
@@ -1,5 +1,7 @@
 package hiccreboot.backend.common.dto.Calendar;
 
+import java.time.LocalDateTime;
+
 import hiccreboot.backend.domain.Schedule;
 import hiccreboot.backend.domain.ScheduleDate;
 import hiccreboot.backend.domain.ScheduleType;
@@ -14,6 +16,8 @@ public class ScheduleDateResponse {
 	private final String content;
 	private final ScheduleType type;
 	private final Long scheduleId;
+	private final LocalDateTime startDateTime;
+	private final LocalDateTime endDateTime;
 
 	public static ScheduleDateResponse create(ScheduleDate scheduleDate) {
 		Schedule schedule = scheduleDate.getSchedule();
@@ -23,6 +27,8 @@ public class ScheduleDateResponse {
 			.content(schedule.getContent())
 			.type(schedule.getScheduleType())
 			.scheduleId(schedule.getId())
+			.startDateTime(schedule.getStartDateTime())
+			.endDateTime(schedule.getEndDateTime())
 			.build();
 	}
 }
