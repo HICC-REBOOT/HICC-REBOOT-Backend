c8a1268295d2c2521814d8a8c0b5952e9a3c936e
leesh <qwqw8019@naver.com>
2024-02-20 16:43:25 +0900

    feat: 댓글 작성 요청에 validation 적용

--- hicc-reboot/src/main/java/hiccreboot/backend/domains/comment/dto/request/PostCommentRequest.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domains/comment/dto/request/PostCommentRequest.java
@@ -1,11 +1,17 @@
 package hiccreboot.backend.domains.comment.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PostCommentRequest {
 
+	@Min(1)
 	private Long articleId;
+	@NotNull
 	private Long parentCommentId;
+	@NotBlank
 	private String content;
 }
