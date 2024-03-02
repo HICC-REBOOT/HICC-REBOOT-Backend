35ea83a6306bf5a1aae3ae5c1cd7747c760d8b98
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2024-03-02 22:55:17 +0900

    refactor: Comment, Article, Schedule의 content 크기 제한 변경 (#153)

--- hicc-reboot/src/main/java/hiccreboot/backend/domains/comment/domain/Comment.java
+++ hicc-reboot/src/main/java/hiccreboot/backend/domains/comment/domain/Comment.java
@@ -1,124 +1,115 @@
 package hiccreboot.backend.domains.comment.domain;
 
-import static hiccreboot.backend.common.consts.MemberConstants.*;
-
-import java.time.LocalDateTime;
-
 import hiccreboot.backend.domains.article.domain.Article;
 import hiccreboot.backend.domains.member.domain.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static hiccreboot.backend.common.consts.MemberConstants.DELETED_MEMBER_NAME;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
-	private final String BLANK_CONTENT = "댓글이 삭제되었습니다.";
+    private final String BLANK_CONTENT = "댓글이 삭제되었습니다.";
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "COMMENT_ID")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+    private Long id;
 
-	@Column(nullable = true)
-	private Long parentCommentId;
+    @Column(nullable = true)
+    private Long parentCommentId;
 
-	@Column(nullable = false)
-	private Boolean isDeleted;
+    @Column(nullable = false)
+    private Boolean isDeleted;
 
-	@Column(nullable = false)
-	private LocalDateTime date;
+    @Column(nullable = false)
+    private LocalDateTime date;
 
-	@ManyToOne
-	@JoinColumn(name = "MEMBER_ID")
-	private Member member;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
-	@Column(nullable = false)
-	private String memberName;
+    @Column(nullable = false)
+    private String memberName;
 
-	@Column(name = "COMMENT_GRADE")
-	@Enumerated(EnumType.STRING)
-	private CommentGrade commentGrade;
+    @Column(name = "COMMENT_GRADE")
+    @Enumerated(EnumType.STRING)
+    private CommentGrade commentGrade;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ARTICLE_ID")
-	private Article article;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ARTICLE_ID")
+    private Article article;
 
-	@Column(nullable = false)
-	private String content;
+    @Column(nullable = false, length = 250)
+    private String content;
 
-	@Builder(access = AccessLevel.PRIVATE)
-	private Comment(Long parentCommentId, Boolean isDeleted, LocalDateTime date, Member member, String memberName,
-		CommentGrade commentGrade, Article article,
-		String content) {
-		this.parentCommentId = parentCommentId;
-		this.isDeleted = isDeleted;
-		this.date = date;
-		this.member = member;
-		this.memberName = memberName;
-		this.commentGrade = commentGrade;
-		addArticle(article);
-		this.content = content;
-	}
+    @Builder(access = AccessLevel.PRIVATE)
+    private Comment(Long parentCommentId, Boolean isDeleted, LocalDateTime date, Member member, String memberName,
+                    CommentGrade commentGrade, Article article,
+                    String content) {
+        this.parentCommentId = parentCommentId;
+        this.isDeleted = isDeleted;
+        this.date = date;
+        this.member = member;
+        this.memberName = memberName;
+        this.commentGrade = commentGrade;
+        addArticle(article);
+        this.content = content;
+    }
 
-	public static Comment createParentComment(Member member, CommentGrade commentGrade, Article article,
-		String content) {
-		return Comment.builder()
-			.parentCommentId(-1L)
-			.isDeleted(false)
-			.member(member)
-			.memberName(member.getName())
-			.commentGrade(commentGrade)
-			.article(article)
-			.date(LocalDateTime.now())
-			.content(content)
-			.build();
-	}
+    public static Comment createParentComment(Member member, CommentGrade commentGrade, Article article,
+                                              String content) {
+        return Comment.builder()
+                .parentCommentId(-1L)
+                .isDeleted(false)
+                .member(member)
+                .memberName(member.getName())
+                .commentGrade(commentGrade)
+                .article(article)
+                .date(LocalDateTime.now())
+                .content(content)
+                .build();
+    }
 
-	public static Comment createChildComment(Long parentCommentId, Member member, CommentGrade commentGrade,
-		Article article, String content) {
-		return Comment.builder()
-			.parentCommentId(parentCommentId)
-			.isDeleted(false)
-			.member(member)
-			.memberName(member.getName())
-			.commentGrade(commentGrade)
-			.article(article)
-			.date(LocalDateTime.now())
-			.content(content)
-			.build();
-	}
+    public static Comment createChildComment(Long parentCommentId, Member member, CommentGrade commentGrade,
+                                             Article article, String content) {
+        return Comment.builder()
+                .parentCommentId(parentCommentId)
+                .isDeleted(false)
+                .member(member)
+                .memberName(member.getName())
+                .commentGrade(commentGrade)
+                .article(article)
+                .date(LocalDateTime.now())
+                .content(content)
+                .build();
+    }
 
-	private void addArticle(Article article) {
-		this.article = article;
-		article.getComments().add(this);
-	}
+    private void addArticle(Article article) {
+        this.article = article;
+        article.getComments().add(this);
+    }
 
-	public void deleteCommentSoftly() {
-		this.member = null;
-		this.memberName = DELETED_MEMBER_NAME;
-		this.isDeleted = true;
-		commentGrade = CommentGrade.NORMAL;
-	}
+    public void deleteCommentSoftly() {
+        this.member = null;
+        this.memberName = DELETED_MEMBER_NAME;
+        this.isDeleted = true;
+        commentGrade = CommentGrade.NORMAL;
+    }
 
-	public void deleteCommentSoftlyWithContent() {
-		this.member = null;
-		this.memberName = DELETED_MEMBER_NAME;
-		this.isDeleted = true;
-		commentGrade = CommentGrade.NORMAL;
-		content = BLANK_CONTENT;
-	}
+    public void deleteCommentSoftlyWithContent() {
+        this.member = null;
+        this.memberName = DELETED_MEMBER_NAME;
+        this.isDeleted = true;
+        commentGrade = CommentGrade.NORMAL;
+        content = BLANK_CONTENT;
+    }
 }
